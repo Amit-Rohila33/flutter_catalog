714176fcb7b8f5293ad42c1d39f0b5a94942f024
Amit Kumar <103894389+Amit-Rohila33@users.noreply.github.com>
2023-01-03 02:03:11 +0530

    login page and page navigation done

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        child: Center(
+      child: Container(
+        child: Text("Messi is the best footballer2"),
+      ),
+    ));
+  }
+}
