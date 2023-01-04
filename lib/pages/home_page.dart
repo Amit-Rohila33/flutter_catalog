c5802366e23cb43671da4ae5546608d05fc0ec16
Amit Kumar <103894389+Amit-Rohila33@users.noreply.github.com>
2023-01-04 12:28:37 +0530

    added app bar and drawer to home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  final String App_name = "Catalog";
   @override
   Widget build(BuildContext context) {
-    return Material(
-        child: Center(
-      child: Container(
-        child: Text("Messi is the best footballer2"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Flutter Catalog",
+          style: TextStyle(color: Colors.white),
+        ),
       ),
-    ));
+      body: Center(
+        child: Container(
+          child: Text("Welcome to our $App_name App"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
   }
 }
